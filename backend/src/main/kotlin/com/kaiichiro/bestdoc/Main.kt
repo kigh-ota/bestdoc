d0e4f8fc8d9fdc16ef50823df9968bf929dfde67
Kaiichiro Ota <kigh.ota@gmail.com>
2021-09-24 02:28:25 +0900

    enable HTTPS

--- backend/src/main/kotlin/com/kaiichiro/bestdoc/Main.kt
+++ backend/src/main/kotlin/com/kaiichiro/bestdoc/Main.kt
@@ -2,12 +2,16 @@ package com.kaiichiro.bestdoc
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.repository.CrudRepository
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.stereotype.Controller
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import java.time.OffsetDateTime
@@ -16,6 +20,8 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.servlet.http.HttpServletRequest
+
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -54,5 +60,19 @@ class NoteController(private val noteRepository: NoteRepository) {
     @QueryMapping
     fun getNote(@Argument id: Long): Note {
         return noteRepository.findById(id).orElseThrow { RuntimeException() }
+    }
+}
+
+@Configuration
+class WebSecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(http: HttpSecurity) {
+        http.requiresChannel()
+            .requestMatchers(RequestMatcher { r: HttpServletRequest ->
+                r.getHeader(
+                    "X-Forwarded-Proto"
+                ) != null
+            })
+            .requiresSecure()
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic()
     }
 }
