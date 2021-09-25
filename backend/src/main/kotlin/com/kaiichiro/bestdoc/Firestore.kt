cc31a7a414c4021cde374c9c218fa4f8c1f2fd98
Kaiichiro Ota <kigh.ota@gmail.com>
2021-09-26 00:10:27 +0900

    get note(s) and refactor

---
+++ backend/src/main/kotlin/com/kaiichiro/bestdoc/Firestore.kt
@@ -0,0 +1,91 @@
+package com.kaiichiro.bestdoc
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.ServiceOptions
+import com.google.cloud.firestore.DocumentSnapshot
+import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.FirestoreOptions
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
+
+@Configuration
+class FirestoreConfiguration {
+    @Bean
+    fun firestore(): Firestore {
+        val path = Path.of(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
+        Files.deleteIfExists(path)
+        Files.createFile(path);
+        Files.writeString(
+            path,
+            System.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"),
+            StandardOpenOption.WRITE
+        )
+        val firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId(
+            ServiceOptions.getDefaultProjectId()
+        )
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .build()
+        return firestoreOptions.service
+    }
+}
+
+@Component
+class FirestoreNoteRepository(private val db: Firestore) : NoteRepository {
+    companion object {
+        private const val COLLECTION = "notes"
+        private val FORMATTER = DateTimeFormatterBuilder()
+            .append(DateTimeFormatter.ISO_LOCAL_DATE)
+            .appendLiteral('T')
+            .appendValue(ChronoField.HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+            .appendLiteral(':')
+            .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+            .appendOffsetId()
+            .toFormatter()
+    }
+
+    override fun findAll(): Iterable<Note> {
+        val query = db.collection(COLLECTION).get()
+        return query.get().documents.map(::transform)
+    }
+
+    override fun findById(id: NoteId): Note {
+        val docRef = db.collection(COLLECTION).document(id)
+        val doc = docRef.get().get()
+        return transform(doc)
+    }
+
+    private fun transform(doc: DocumentSnapshot): Note {
+        if (doc.data == null) {
+            throw RuntimeException()
+        }
+        return Note(
+            doc.id,
+            doc.get("title") as String,
+            doc.get("text") as String,
+            OffsetDateTime.parse(doc.get("createdAt") as String, FORMATTER),
+            OffsetDateTime.parse(doc.get("updatedAt") as String, FORMATTER)
+        )
+    }
+
+    override fun save(note: Note): NoteId {
+        val result = db.collection(COLLECTION).add(object {
+            val title = note.title
+            val text = note.text
+            val createdAt = note.createdAt.format(FORMATTER)
+            val updatedAt = note.updatedAt.format(FORMATTER)
+        })
+        val id = result.get().id
+        System.out.println("id : " + id)
+        return id
+    }
+}
--- backend/src/main/kotlin/com/kaiichiro/bestdoc/Main.kt
+++ backend/src/main/kotlin/com/kaiichiro/bestdoc/Main.kt
@@ -1,12 +1,7 @@
 package com.kaiichiro.bestdoc
 
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.cloud.ServiceOptions
-import com.google.cloud.firestore.Firestore
-import com.google.cloud.firestore.FirestoreOptions
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.graphql.data.method.annotation.Argument
@@ -16,42 +11,13 @@ import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.util.matcher.RequestMatcher
-import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.StandardOpenOption
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeFormatterBuilder
-import java.time.temporal.ChronoField.*
-import java.util.*
-import javax.servlet.http.HttpServletRequest
 
 
 @SpringBootApplication
-class BestDocApplication {
-
-    @Bean
-    fun firestore(): Firestore {
-        val path = Path.of(System.getenv("GOOGLE_APPLICATION_CREDENTIALS"));
-        Files.deleteIfExists(path)
-        Files.createFile(path);
-        Files.writeString(
-            path,
-            System.getenv("GOOGLE_APPLICATION_CREDENTIALS_JSON"),
-            StandardOpenOption.WRITE
-        )
-        val firestoreOptions = FirestoreOptions.getDefaultInstance().toBuilder().setProjectId(
-            ServiceOptions.getDefaultProjectId()
-        )
-            .setCredentials(GoogleCredentials.getApplicationDefault())
-            .build()
-        return firestoreOptions.service
-    }
-}
+class BestDocApplication
 
 fun main(args: Array<String>) {
     runApplication<BestDocApplication>(*args)
@@ -72,47 +38,12 @@ class Note(
     }
 }
 
+typealias NoteId = String
+
 interface NoteRepository {
-    fun findAll(): List<Note>
-    fun findById(id: Long): Optional<Note>
-    fun save(note: Note): String
-}
-
-@Component
-class FirestoreNoteRepository(private val db: Firestore) : NoteRepository {
-    companion object {
-        private const val COLLECTION = "notes"
-        private val FORMATTER = DateTimeFormatterBuilder()
-            .append(DateTimeFormatter.ISO_LOCAL_DATE)
-            .appendLiteral('T')
-            .appendValue(HOUR_OF_DAY, 2)
-            .appendLiteral(':')
-            .appendValue(MINUTE_OF_HOUR, 2)
-            .appendLiteral(':')
-            .appendValue(SECOND_OF_MINUTE, 2)
-            .appendOffsetId()
-            .toFormatter()
-    }
-
-    override fun findAll(): List<Note> {
-        return listOf() // TODO
-    }
-
-    override fun findById(id: Long): Optional<Note> {
-        return Optional.empty() // TODO
-    }
-
-    override fun save(note: Note): String {
-        val result = db.collection(COLLECTION).add(object {
-            val title = note.title
-            val text = note.text
-            val createdAt = note.createdAt.format(FORMATTER)
-            val updatedAt = note.updatedAt.format(FORMATTER)
-        })
-        val id = result.get().id
-        System.out.println("id : " + id)
-        return id
-    }
+    fun findAll(): Iterable<Note>
+    fun findById(id: NoteId): Note
+    fun save(note: Note): NoteId
 }
 
 @Controller
@@ -120,13 +51,13 @@ class FirestoreNoteRepository(private val db: Firestore) : NoteRepository {
 class NoteController(private val noteRepository: NoteRepository) {
 
     @QueryMapping
-    fun allNotes(): List<Note> {
-        return noteRepository.findAll().toList()
+    fun allNotes(): Iterable<Note> {
+        return noteRepository.findAll()
     }
 
     @QueryMapping
-    fun getNote(@Argument id: Long): Note {
-        return noteRepository.findById(id).orElseThrow { RuntimeException() }
+    fun getNote(@Argument id: NoteId): Note {
+        return noteRepository.findById(id)
     }
 
     @MutationMapping
@@ -142,7 +73,7 @@ class ProductionWebSecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.csrf().disable()
         http.requiresChannel()
-            .requestMatchers(RequestMatcher { r: HttpServletRequest ->
+            .requestMatchers({ r ->
                 r.getHeader(
                     "X-Forwarded-Proto"
                 ) != null
