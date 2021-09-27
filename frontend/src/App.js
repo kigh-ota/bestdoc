b017d8048464d19a8f9156028b8924952d7f217a
Kaiichiro Ota <kigh.ota@gmail.com>
2021-09-27 23:43:35 +0900

    state transitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,11 +10,22 @@ function allNotesToObject(notes) {
   return obj;
 }
 
+const NEW_NOTE = {
+  id: null,
+  title: "",
+  text: "",
+};
+
 export function App() {
-  const [selectedId, setSelectedId] = useState(null);
+  const [editor, setEditor] = useState(NEW_NOTE);
   const [notes, setNotes] = useState({}); // {id: Note}
 
-  function getNoteAndUpdateState(id) {
+  const isChanged = () => {
+    const base = editor.id === null ? NEW_NOTE : notes[editor.id];
+    return editor.title !== base.title || editor.text !== base.text;
+  };
+
+  const getNoteAndUpdateState = (id) => {
     return fetch("/graphql", {
       method: "POST",
       headers: {
@@ -31,7 +42,7 @@ export function App() {
         const note = data.data.getNote;
         setNotes({ ...notes, [note.id]: note });
       });
-  }
+  };
 
   useEffect(() => {
     fetch("/graphql", {
@@ -51,86 +62,133 @@ export function App() {
       });
   }, []);
 
-  function updateTitle(title) {
-    if (selectedId === null) {
-      return; // fail safe
-    }
-    const note = notes[selectedId];
-    setNotes({ ...notes, [selectedId]: { ...note, title } });
-  }
+  const updateTitle = (title) => setEditor({ ...editor, title });
+  const updateText = (text) => setEditor({ ...editor, text });
 
-  function updateEditorText(text) {
-    if (selectedId === null) {
-      return; // fail safe
+  const saveIfChangedAndUpdateState = async () => {
+    if (!isChanged()) {
+      return Promise.resolve(NEW_NOTE);
     }
-    const note = notes[selectedId];
-    setNotes({ ...notes, [selectedId]: { ...note, text } });
-  }
-
-  function save() {
-    const note = notes[selectedId];
-    fetch("/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        query: `mutation { updateNote(id:"${selectedId}", title:"${note.title}", text:"""${note.text}""") { id, title, text, createdAt, updatedAt } }`,
-      }),
-    }).then((r) => r.json())
-    .then((data) => {
-      console.log("data returned:", data);
-      const note = data.data.updateNote;
-      setNotes({ ...notes, [note.id]: note });
-    })
-  }
+    if (editor.id === null) {
+      // Add
+      return fetch("/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query: `mutation { addNote(title:"${editor.title}", text:"""${editor.text}""") { id, title, text, createdAt, updatedAt } }`,
+        }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          console.log("data returned:", data);
+          const addedNote = data.data.addNote;
+          setNotes({ ...notes, [addedNote.id]: addedNote });
+          return addedNote;
+        });
+    } else {
+      // Update
+      return fetch("/graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query: `mutation { updateNote(id:"${editor.id}", title:"${editor.title}", text:"""${editor.text}""") { id, title, text, createdAt, updatedAt } }`,
+        }),
+      })
+        .then((r) => r.json())
+        .then((data) => {
+          console.log("data returned:", data);
+          const updatedNote = data.data.updateNote;
+          setNotes({ ...notes, [updatedNote.id]: updatedNote });
+          return updatedNote;
+        });
+    }
+  };
 
   return (
     <div id="App">
-      <div class="containers">
+      <div className="containers">
         <div id="drawer">
           <div id="drawer-toolbar">
-            <button>New</button>
-            <button onClick={save} disabled={selectedId === null}>
-              Save
+            <button
+              onClick={async () => {
+                await saveIfChangedAndUpdateState();
+                setEditor(NEW_NOTE);
+              }}
+            >
+              New
+            </button>
+            <button
+              onClick={async () => {
+                if (editor.id === null) {
+                  // Add
+                  const addedNote = await saveIfChangedAndUpdateState();
+                  setEditor(addedNote);
+                } else {
+                  // Update
+                  saveIfChangedAndUpdateState();
+                }
+              }}
+            >
+              {editor.id === null ? "Add" : "Update"}
             </button>
           </div>
           <NoteList
             notes={notes}
-            selectedId={selectedId}
+            selectedId={editor.id}
             onSelect={async (id) => {
-              setSelectedId(id);
-              getNoteAndUpdateState(id);
+              await saveIfChangedAndUpdateState();
+              setEditor(notes[id]);
             }}
           />
         </div>
         <div id="content">
-          <input
-            id="note-title-input"
-            type="text"
-            value={selectedId ? notes[selectedId].title : ""}
+          <TitleInput
+            value={editor.title}
             onChange={(e) => updateTitle(e.target.value)}
-            disabled={selectedId === null}
-            placeholder="Title"
           />
           <div id="note-text-container">
-            <textarea
-              id="note-text-input"
-              onChange={(e) => updateEditorText(e.target.value)}
-              value={selectedId ? notes[selectedId].text : ""}
-              disabled={selectedId === null}
+            <TextInput
+              value={editor.text}
+              onChange={(e) => updateText(e.target.value)}
             />
-            <div
-              id="note-text-preview"
-              dangerouslySetInnerHTML={{
-                __html: marked(selectedId ? notes[selectedId].text : ""),
-              }}
-            ></div>
+            <Preview text={editor.text} />
           </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function TitleInput({ value, onChange }) {
+  return (
+    <input
+      id="note-title-input"
+      type="text"
+      value={value}
+      onChange={onChange}
+      placeholder="Title"
+    />
+  );
+}
+
+function TextInput({ value, onChange }) {
+  return <textarea id="note-text-input" value={value} onChange={onChange} />;
+}
+
+function Preview({ text }) {
+  return (
+    <div
+      id="note-text-preview"
+      dangerouslySetInnerHTML={{
+        __html: marked(text),
+      }}
+    ></div>
   );
 }
 
@@ -155,10 +213,10 @@ function NoteList({ notes, onSelect, selectedId }) {
           return (
             <li
               key={note.id}
-              class={className}
+              className={className}
               onClick={(e) => onSelect(note.id)}
             >
-              {note.title}
+              {note.title || "(No title)"}
             </li>
           );
         })}
